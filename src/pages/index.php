f0f961dbfee1821933e8ff04f38c12e4f3447cc9
Luca Foglieni <l.foglieni5@studenti.unibg.it>
2024-06-03 17:35:34 +0200

    Update della landing page con animazioni e css apposito

--- src/pages/index.php
+++ src/pages/index.php
@@ -7,14 +7,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Landing Page</title>
   <link rel="stylesheet" href="../css/style.css" />
+  <link rel="stylesheet" href="../css/landingPage.css" />
   <link rel="icon" href="../icons/road.png" type="image/x-icon" />
 </head>
 
 <body id="body">
 
   <div id="landingPageTitleMain">
-    <h1 id="landingPageTitle">DATABASE AUTOSTRADE</h1>
-
     <nav class="roundedElement" id="landingPageNav">
       <ul>
         <li><a href="index.php">Home</a></li>
@@ -23,6 +22,19 @@
         <li><a href="autostrada.php">Autostrada</a></li>
       </ul>
     </nav>
+
+    <div id="landingPageTitleDiv">
+      <div id="landingPageTitleInnerDiv">
+        <h1 id="landingPageTitle">DATABASE AUTOSTRADE</h1>
+        <!-- <h1 id="landingPageTitleShadow">DATABASE AUTOSTRADE</h1> -->
+      </div>
+      <div>
+        <div class="ball"></div>
+      </div>
+    </div>
+
+
+
 
   </div>
 
