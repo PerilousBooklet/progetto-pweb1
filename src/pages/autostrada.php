e5ec948580b3868a9c60121720f2140a3d2ada3e
Luca-Foglieni <l.foglieni5@studenti.unibg.it>
2024-06-09 16:11:01 +0200

    fix autostrade

--- src/pages/autostrada.php
+++ src/pages/autostrada.php
@@ -3,68 +3,77 @@
 <html lang="it">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Database Autostrada</title>
-    <link rel="stylesheet" href="../css/style.css" />
-    <link rel="icon" href="../icons/road.png" type="image/x-icon" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Database Autostrada</title>
+	<link rel="stylesheet" href="../css/style.css" />
+
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+		rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&display=swap" rel="stylesheet">
+
+	<!-- Icons -->
+	<link rel="icon" href="../icons/road.png" type="image/x-icon" />
 </head>
 
 <body>
 
-    <?php
-    include 'custom-lib.php';
-    include 'header.html';
-    include 'nav.html';
-    ?>
+	<?php
+	include 'custom-lib.php';
+	include 'header.html';
+	include 'nav.html';
+	?>
 
-    <main id="row">
-        <!-- Form Menu -->
-        <div id="filterPanel" class="roundedElement">
-            <div id="codiceNazione">
-                <label for="codiceNazione">Codice Nazione</label>
-                <br>
-                <input type="text">
-            </div>
+	<main id="row">
+		<!-- Form Menu -->
+		<div id="filterPanel" class="roundedElement">
+			<div id="codiceNazione">
+				<label for="codiceNazione">Codice Nazione</label>
+				<br>
+				<input type="text">
+			</div>
 
-            <br>
+			<br>
 
-            <div id="codiceEuropeoAutostrada">
-                <label for="codiceEuropeo">Codice Europeo</label>
-                <br>
-                <input type="text">
-            </div>
+			<div id="codiceEuropeoAutostrada">
+				<label for="codiceEuropeo">Codice Europeo</label>
+				<br>
+				<input type="text">
+			</div>
 
-            <br>
+			<br>
 
-            <div id="nomeAutostrada">
-                <label for="nomeAutostrada">Nome Autostrada</label>
-                <br>
-                <input type="text">
-            </div>
+			<div id="nomeAutostrada">
+				<label for="nomeAutostrada">Nome Autostrada</label>
+				<br>
+				<input type="text">
+			</div>
 
-            <br>
+			<br>
 
-            <div id="lunghezzaAutostrada">
-                <label for="lunghezzaAutostrada">Lunghezza Autostrada</label>
-                <br>
-                <input type="text">
-            </div>
+			<div id="lunghezzaAutostrada">
+				<label for="lunghezzaAutostrada">Lunghezza Autostrada</label>
+				<br>
+				<input type="text">
+			</div>
 
-        </div>
+		</div>
 
-        <div id="queryResult" class="roundedElement">
-            <h2>Risultato:</h2>
-            <?php
-            table_gen("Autostrada");
-            ?>
-        </div>
+		<div id="queryResult" class="roundedElement">
+			<h2>Risultato:</h2>
+			<?php
+			table_gen("Autostrada");
+			?>
+		</div>
 
-    </main>
+	</main>
 
-    <?php
-    include 'footer.html';
-    ?>
+	<?php
+	include 'footer.html';
+	?>
 
 </body>
 
