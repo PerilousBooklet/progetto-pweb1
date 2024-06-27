154b2f6516596bccd5e19af4af5b87fea9f97fde
socket772 <paolotto.ghilardi@gmail.com>
2024-06-27 11:57:18 +0200

    Avanzo ricerca php

--- src/pages/custom-lib.php
+++ src/pages/custom-lib.php
@@ -36,11 +36,140 @@ function database_connection_v2() {
 function table_gen(String $tabella)
 {
 
-	if (!isset($_POST)) {
-		print_no_search($tabella);
+	if (empty($_POST)) {
+		echo "$_POST<br/>";
+		print_no_search($tabella, true);
 	} else {
-		print_yes_search($tabella, true);
+		var_dump($_POST);
+		print_yes_search($tabella);
 	}
+
+	return;
+}
+
+function print_yes_search(String $tabella) {
+
+	$sql_starter = "SELECT * FROM $tabella WHERE";
+
+	switch ($tabella) {
+		case 'Comune':
+			$sql = "$sql_starter `codice` = :codice";
+			break;
+		case 'Autostrada':
+			$sql = "$sql_starter `cod_naz` = :cod_naz";
+			break;
+		case 'Casello':
+			$sql = "$sql_starter `cod_naz` LIKE :cod_naz AND `comune` LIKE :comune AND `nome` LIKE :nome AND `x` = LIKE :x AND `y` LIKE :y AND `is_automatico` LIKE :is_automatico AND `data_automazione` LIKE :data_automazione AND `codice` LIKE :codice";
+			break;
+		default:
+			return;
+	}
+
+	return $sql;
+}
+
+function print_no_search(String $tabella, bool $is_search) {
+	// NON TOCCARE A MENO CHE NON SAI COSA STAI FACENDO
+	$tabella_regione = array("sigla", "regione");
+	$visualizza_regione = array("Sigla", "Regione");
+
+	$tabella_provincia = array("sigla", "regione", "nome");
+	$visualizza_provincia = array("Sigla", "Regione", "Nome");
+
+	$tabella_comune = array("codice", "provincia", "nome");
+	$visualizza_comune = array("Codice", "Provincia", "Nome");
+
+	$tabella_autostrada = array("cod_naz", "cod_eu", "nome", "lunghezza");
+	$visualizza_autostrada = array("Codice nazionale", "Codice europeo", "Nome", "Lunghezza");
+
+	$tabella_casello = array("codice", "cod_naz", "comune", "nome", "x", "y", "is_automatico", "data_automazione");
+	$visualizza_casello = array("Codice", "Codice nazionale", "Comune", "Nome", "x", "y", "Automatico", "Data automazione");
+
+
+	// Selezione della tabella sulla quale lavorare
+	switch ($tabella) {
+		case 'Regione':
+			$tabella_default = $tabella_regione;
+			$visualizza_default = $visualizza_regione;
+			break;
+		case 'Provincia':
+			$tabella_default = $tabella_provincia;
+			$visualizza_default = $visualizza_provincia;
+			break;
+		case 'Comune':
+			$tabella_default = $tabella_comune;
+			$visualizza_default = $visualizza_comune;
+			break;
+		case 'Autostrada':
+			$tabella_default = $tabella_autostrada;
+			$visualizza_default = $visualizza_autostrada;
+			break;
+		case 'Casello':
+			$tabella_default = $tabella_casello;
+			$visualizza_default = $visualizza_casello;
+			break;
+		default:
+			return;
+	}
+
+	// Apertura connessione verso il database
+	$conn = database_connection_v2();
+	// Query di recupero dati dal db
+
+	if (!$is_search) {
+		$sql = print_yes_search($tabella);
+		$stmt = $conn->prepare($sql);
+		$data_array = $_POST;
+		$stmt->execute($data_array);
+	} else {
+		$sql = "SELECT * FROM `$tabella`";
+		$stmt = $conn->query($sql);
+	}
+
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	if(empty(array_filter($result))) {
+		echo "Tabella vuota";
+		return;
+	}
+
+	// INIZIO STAMPA DELLA TABELLA
+	echo "<table class=''>";
+
+	// Header della tabella
+	echo "<thead class='rs-head'>";
+
+	foreach ($visualizza_default as $element) {
+		echo "<th class='rs-header'>$element</th>";
+	}
+
+	echo "</thead>";
+
+	// Stampa delle righe della tabella
+	switch ($tabella) {
+		case 'Regione':
+			while_regione($result);
+			break;
+		case 'Provincia':
+			while_provincia($result);
+			break;
+		case 'Comune':
+			while_comune($result);
+			break;
+		case 'Autostrada':
+			while_autostrada($result);
+			break;
+		case 'Casello':
+			while_casello($result);
+			break;
+		default:
+			return;
+	}
+	echo "</table>";
+
+	// Chuido la connessione con il db
+	$stmt = null;
+	$conn = null;
 
 	return;
 }
@@ -251,139 +380,4 @@ function inserimento(String $tabella, Array $data_array) {
 
 	return;
 }
-
-function print_yes_search(String $tabella) {
-
-	$sql_starter = "SELECT * FROM $tabella WHERE";
-
-	switch ($tabella) {
-		case 'Comune':
-			$sql = "$sql_starter `codice` = :codice";
-			break;
-		case 'Autostrada':
-			$sql = "$sql_starter `cod_naz` = :cod_naz";
-			break;
-		case 'Casello':
-			$sql = "$sql_starter `cod_naz` LIKE :cod_naz AND `comune` LIKE :comune AND `nome` LIKE :nome AND `x` = LIKE :x AND `y` LIKE :y AND `is_automatico` LIKE :is_automatico AND `data_automazione` LIKE :data_automazione AND `codice` LIKE :codice";
-			break;
-		default:
-			return;
-	}
-
-	// Esecuzione query
-	$conn = database_connection_v2();
-
-	$stmt = $conn->prepare($sql);
-
-	$stmt->execute($_POST);
-
-	$result = $stmt->fetchAll();
-
-	return $result;
-}
-
-function print_no_search(String $tabella, bool $is_search) {
-	// NON TOCCARE A MENO CHE NON SAI COSA STAI FACENDO
-	$tabella_regione = array("sigla", "regione");
-	$visualizza_regione = array("Sigla", "Regione");
-
-	$tabella_provincia = array("sigla", "regione", "nome");
-	$visualizza_provincia = array("Sigla", "Regione", "Nome");
-
-	$tabella_comune = array("codice", "provincia", "nome");
-	$visualizza_comune = array("Codice", "Provincia", "Nome");
-
-	$tabella_autostrada = array("cod_naz", "cod_eu", "nome", "lunghezza");
-	$visualizza_autostrada = array("Codice nazionale", "Codice europeo", "Nome", "Lunghezza");
-
-	$tabella_casello = array("codice", "cod_naz", "comune", "nome", "x", "y", "is_automatico", "data_automazione");
-	$visualizza_casello = array("Codice", "Codice nazionale", "Comune", "Nome", "x", "y", "Automatico", "Data automazione");
-
-
-	// Selezione della tabella sulla quale lavorare
-	switch ($tabella) {
-		case 'Regione':
-			$tabella_default = $tabella_regione;
-			$visualizza_default = $visualizza_regione;
-			break;
-		case 'Provincia':
-			$tabella_default = $tabella_provincia;
-			$visualizza_default = $visualizza_provincia;
-			break;
-		case 'Comune':
-			$tabella_default = $tabella_comune;
-			$visualizza_default = $visualizza_comune;
-			break;
-		case 'Autostrada':
-			$tabella_default = $tabella_autostrada;
-			$visualizza_default = $visualizza_autostrada;
-			break;
-		case 'Casello':
-			$tabella_default = $tabella_casello;
-			$visualizza_default = $visualizza_casello;
-			break;
-		default:
-			return;
-	}
-
-	// Apertura connessione verso il database
-	$conn = database_connection_v2($tabella);
-	// Query di recupero dati dal db
-	$sql = "SELECT * FROM `$tabella`";
-
-	$stmt = $conn->query($sql);
-
-	if ($is_search) {
-		$result = print_yes_search();
-	} else {
-		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}
-
-
-	if(empty(array_filter($result))) {
-		echo "Tabella vuota";
-		return;
-	}
-
-	// INIZIO STAMPA DELLA TABELLA
-	echo "<table class=''>";
-
-	// Header della tabella
-	echo "<thead class='rs-head'>";
-
-	foreach ($visualizza_default as $element) {
-		echo "<th class='rs-header'>$element</th>";
-	}
-
-	echo "</thead>";
-
-	// Stampa delle righe della tabella
-	switch ($tabella) {
-		case 'Regione':
-			while_regione($result);
-			break;
-		case 'Provincia':
-			while_provincia($result);
-			break;
-		case 'Comune':
-			while_comune($result);
-			break;
-		case 'Autostrada':
-			while_autostrada($result);
-			break;
-		case 'Casello':
-			while_casello($result);
-			break;
-		default:
-			return;
-	}
-	echo "</table>";
-
-	// Chuido la connessione con il db
-	$stmt = null;
-	$conn = null;
-
-	return;
-}
-
 ?>
